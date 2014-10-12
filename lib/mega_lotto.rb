1e23929fc63512c3591f85879083b21833628636
elitenomad <stalin.pranava@gmail.com>
2014-10-12 12:22:35 +1100

    Mega_lotto Gem preparation

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,19 @@
+require "mega_lotto/version"
+
+begin
+  require "pry"
+rescue LoadError
+end
+
+module MegaLotto
+ class Drawing
+  def draw
+    6.times.map { draw_num }
+  end
+
+  private
+   def draw_num
+     rand(0...60)
+   end
+ end
+end
